7573eb8900206e68d3f8e7fb8e206ce60eec4441
thejaswinikn123-glitch <thejaswini.kn123@gmail.com>
2026-07-20 23:28:17 +0530

    demoqa.jon

---
+++ tests/demoqa.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+import data from "../testdata/demoqa.json"
+ 
+test('test box', async ({ page }) => {
+
+    await page.goto('https://demoqa.com/text-box')
+
+    await page.locator('#userName').fill(data.fullname)
+
+    await page.locator('input[placeholder="name@example.com"]').fill(data.emailaddress)
+  
+    await page.locator('#CurrentAddress').fill(data.currentaddress)
+
+    await page.locator('#permanentAddress').fill(data.parmanentadress)
+
+    await page.locator('#submit').click()
+
+    await expect(page.locator('#name')).toContainText(data.fullname)
+
+});
+
+
+//input[@placeholder="Full Name"]
+//input[@id="userName"]
+// #userName
