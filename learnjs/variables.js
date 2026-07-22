b0051f97f15d3070fd9aa3ce09d50b5dd3263a4a
thejaswinikn123-glitch <thejaswini.kn123@gmail.com>
2026-07-22 22:12:04 +0530

    variables.js

---
+++ learnjs/variables.js
@@ -0,0 +1,49 @@
+
+// var studentname = "Prashi"
+
+// studentname = "vana"
+
+// console.log(studentname)
+
+//let studentname = "Rathna"
+
+//studentname = "Riyu"
+
+//console.log(studentname)
+
+
+// const studentname = "Theju"
+
+// console.log(studentname)
+
+// const studentname = "Nagu"
+
+// console.log(studentname)
+
+
+// if(true){
+
+//  const traineename = "Theju"
+
+// console.log(traineename)
+
+// }
+
+// if(true){
+
+// console.log(traineename)
+
+// }
+
+if(true){
+    
+    const studentname = "Riyansh"
+
+    console.log(studentname)
+
+}
+if(true){
+
+    console.log(studentname)
+
+}
