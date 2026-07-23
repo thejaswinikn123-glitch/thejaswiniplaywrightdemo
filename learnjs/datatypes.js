364b6cca559ca54282e4f7f81a50c29d2890e0ba
thejaswinikn123-glitch <thejaswini.kn123@gmail.com>
2026-07-23 23:06:34 +0530

    datatypes.js

---
+++ learnjs/datatypes.js
@@ -0,0 +1,62 @@
+
+
+// let X = "Theju"
+
+// typeof()
+
+// console.log(typeof(X))
+
+// number
+
+// let X = 10
+
+// typeof()
+
+// console.log(typeof(X))
+
+//  boolean
+
+// let X = true
+
+// typeof()
+
+// console.log(typeof(X))
+
+// String
+
+// let X = "10"
+
+// typeof()
+
+// console.log(typeof(X))
+
+// Null
+
+// x3 = null
+
+// console.log(typeof(x3)) // data type of null is object not a null
+
+//undefined
+
+//e4 =
+// console.log(typeof(e4))
+
+//non-primitive data types
+
+//arrays
+//objects
+//rejex
+//functions
+
+username = "Admin";
+password = "admin123"
+
+
+// await page.locator(usernameXpath).fill(username)
+console.log(username)
+
+Credential = ["Admin", "admin123"]
+
+// await page.locator(usernameXpath).fill(Credential[0])
+console.log(Credential[1])
+
